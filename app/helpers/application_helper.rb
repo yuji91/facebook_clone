46e3a403649cff1df7e22e43803da86f4a2d0247
Yuji <rions.garden@icloud.com>
2018-02-04 18:00:46 +0900

    fix profile_img resize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
+    return image_tag(user.avatar, :size => "80x80", alt: user.name) if user.avatar?
     unless user.provider.blank?
       img_url = user.image_url
     else
